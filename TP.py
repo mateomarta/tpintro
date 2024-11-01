ee2e177835446a3c565f7ef549b973d64722749a
mateomarta <163466552+mateomarta@users.noreply.github.com>
2024-11-01 00:17:38 -0300

    Update TP.py

--- TP.py
+++ TP.py
@@ -1,10 +1,6 @@
 import random
 
-def ingresoProductor(): #falta presentacion
-    nombreApellido=input("Ingrese su nombre y apellido: ")
-    dni=validarRango(10000000,99999999,"Ingrese su dni: ")
-    
-
+#Funciones genericas:
 def validarRango(desde,hasta,texto):
     valor=int(input(texto))
     while valor>hasta or valor<desde:
@@ -12,117 +8,6 @@ def validarRango(desde,hasta,texto):
         valor=int(input(texto))
     return valor
 
-def ingresoCliente(LiDNI,cont,LiMo,LiAu,LiCa,LiMod,LiVa):
-    nombreApellido=input("Ingrese su nombre y apellido: ")
-    Auxdni=validarRango(10000000,99999999,"Ingrese su dni: ")
-    if busqueda(LiDNI,Auxdni) != -1:
-        print("DNI DUPLICADO")
-        Auxdni=validarRango(10000000,99999999,"Ingrese su dni: ")
-    else:
-        LiDNI.append(Auxdni)
-        print("el nro de cliente es: ",1+cont)
-        print("")
-        
-        opcion=validarRango(1,3, "Ingrese 1 si el tipo de vehiculo es una moto, ingrese 2 si es un auto y 3 si es un camion")
-        azar=random.randint(0,5)
-        if opcion==1:
-            print("el modelo de vehiculo es el: ",LiMo[azar])
-            LiMod.append(LiMo[azar])
-            LiVa.append(random.randint(1000000,4000000))
-            print("el precio del vehiculo es: $",LiVa[cont])
-        elif opcion==2:
-            print("el modelo de vehiculo es el: ",LiAu[azar])
-            LiMod.append(LiAu[azar])
-            LiVa.append(random.randint(5000000,10000000))
-            print("el precio del vehiculo es: $",LiVa[cont])
-        else:
-            print("el modelo de vehiculo es el: ",LiCa[azar])
-            LiMod.append(LiCa[azar])
-            LiVa.append(random.randint(8000000,13000000))
-            print("el precio del vehiculo es: $",LiVa[cont])
-#carga lista dni, modelo y el precio
-
-
-def cotizacion(LiVa,LiEm,cont,LiPa):
-    auxval=LiVa[cont]*0.005
-    
-    opcion=validarRango(1,3, "Ingrese 1 si el tipo de seguro es contra tercero, 2 contra tercero completo y 3 para todo riesgo")
-    
-    if opcion == 1:
-        calculo=auxval*1.005
-        print("cotizacion: contra tercero")
-        print("")
-        print("->empresa 1: $",round(calculo*1.008,0))
-        print("->empresa 2: $",round(calculo*1.014,0))
-        print("->empresa 3: $",round(calculo*1.011,0))
-        opcionem=validarRango(1,3, "elija tipo de empresa (1,2,3): ")
-        if opcionem == 1:
-            pagomen=round(calculo*1.08,0)
-
-        elif opcionem == 2:
-            pagomen=round(calculo*1.14,0)
-
-        else:
-            pagomen=round(calculo*1.11,0)
-
-            
-    elif opcion == 2:
-        calculo=auxval*1.010
-        print("cotizacion: contra tercero completo")
-        print("")
-        print("->empresa 1: $",round(calculo*1.022,0))
-        print("->empresa 2: $",round(calculo*1.017,0))
-        print("->empresa 3: $",round(calculo*1.020,0))
-        print("")
-        opcionem=validarRango(1,3, "elija tipo de empresa (1,2,3): ")
-        if opcionem == 1:
-            pagomen=round(calculo*1.22,0)
-
-        elif opcionem == 2:
-            pagomen=round(calculo*1.17,0)
-
-        else:
-            pagomen=round(calculo*1.10,0)
-
-            
-    else:
-        calculo=auxval*1.020
-        print("cotizacion: todo riesgo")
-        print("")
-        print("->empresa 1: $",round(calculo*1.024,0))
-        print("->empresa 2: $",round(calculo*1.027,0))
-        print("->empresa 3: $",round(calculo*1.033,0))
-        print("")
-        opcionem=validarRango(1,3, "elija tipo de empresa (1,2,3): ")
-        if opcionem == 1:
-            pagomen=round(calculo*1.24,0)
-
-        elif opcionem == 2:
-            pagomen=round(calculo*1.27,0)
-
-        else:
-            pagomen=round(calculo*1.33,0)
-
-        
-    LiEm.append(opcionem)
-    LiPa.append(pagomen)
-    
-    print(LiEm[cont])
-    print(LiPa[cont])
-            
-
-    def poliza(LiDni, LiMod, LiVa, LiPa, LiEm):
-    print("")
-    print("")
-    print("==========================================================================")
-    print("***POLIZA***")
-    print("==========================================================================")
-    print("\tDNI        MODELO                              VALOR VEHICULO    PAGO MENSUAL    EMPRESA")
-    
-    for i in range(len(LiDni)):
-        print("\t%-10d %-35s $%14.2f $%13.2f %10s" % (LiDni[i], LiMod[i], LiVa[i], LiPa[i], LiEm[i]))
-    
-               
 def busqueda (lista,valor):
     pos = -1
     i = 0
@@ -132,33 +17,144 @@ def busqueda (lista,valor):
         i+=1
     return pos
 
-    
+##############################################
+def ingresoProductor(): #falta presentacion
+    nombreApellido=input("Ingrese su nombre y apellido: ")
+    dni=validarRango(10000000,99999999,"Ingrese su dni: ")
 
+def ingresoCliente(LiDNI,cont,LiMo,LiAu,LiCa,LiMod,LiVa):
+    #Ingreso y validacion del DNI (por rango y duplicado)
+    nombreApellido=input("Ingrese su nombre y apellido: ")
+    Auxdni=validarRango(10000000,99999999,"Ingrese su dni: ")
+    while busqueda(LiDNI,Auxdni) != -1:
+        print("DNI DUPLICADO")
+        Auxdni=validarRango(10000000,99999999,"Ingrese su dni: ")
+    LiDNI.append(Auxdni)
+    #
+    print("el nro de cliente es: ",1+cont)
+    print("")
+    #Ingreso tipo de veiculo
+    opcion=validarRango(1,3, "Ingrese 1 si el tipo de vehiculo es una moto, ingrese 2 si es un auto y 3 si es un camion")
+    #Se asigna un modelo y valor random en base al tipo de vehiculo
+    azar=random.randint(0,5)
+    if opcion==1:
+        print("el modelo de vehiculo es el: ",LiMo[azar])
+        LiMod.append(LiMo[azar])
+        LiVa.append(random.randint(1000000,4000000))
+        print("el precio del vehiculo es: $",LiVa[cont])
+    elif opcion==2:
+        print("el modelo de vehiculo es el: ",LiAu[azar])
+        LiMod.append(LiAu[azar])
+        LiVa.append(random.randint(5000000,10000000))
+        print("el precio del vehiculo es: $",LiVa[cont])
+    else:
+        print("el modelo de vehiculo es el: ",LiCa[azar])
+        LiMod.append(LiCa[azar])
+        LiVa.append(random.randint(8000000,13000000))
+        print("el precio del vehiculo es: $",LiVa[cont])
+#carga lista dni, modelo y el precio
+
+def cotizacion(LiVa,LiEm,cont,LiPa):
+    auxval=LiVa[cont]*0.001
+    #Elegir tipo de seguro
+    opcion=validarRango(1,3, "Ingrese 1 si el tipo de seguro es contra tercero, 2 contra tercero completo y 3 para todo riesgo")
+    #En base al tipo de seguro se imprime el coste que tendria segun cada empresa
+    #El primer if representa el tipo de seguro, el segundo if la empresa
+    if opcion == 1:
+        auxval = auxval * 5
+        print("cotizacion: contra tercero")
+        print("")
+        print("->empresa 1: $",round(auxval*1,0))
+        print("->empresa 2: $",round(auxval*1.25,0))
+        print("->empresa 3: $",round(auxval*1.5,0))
+        opcionem=validarRango(1,3, "elija tipo de empresa (1,2,3): ")
+        if opcionem == 1:
+            pagomen=round(auxval*1,0)
+        elif opcionem == 2:
+            pagomen=round(auxval*1.25,0)
+        else:
+            pagomen=round(auxval*1.5,0)
+    elif opcion == 2:
+        auxval = auxval * 7
+        print("cotizacion: contra tercero completo")
+        print("")
+        print("->empresa 1: $",round(auxval*1.5,0))
+        print("->empresa 2: $",round(auxval*1,0))
+        print("->empresa 3: $",round(auxval*1.25,0))
+        print("")
+        opcionem=validarRango(1,3, "elija tipo de empresa (1,2,3): ")
+        if opcionem == 1:
+            pagomen=round(auxval*1.5,0)
+        elif opcionem == 2:
+            pagomen=round(auxval*1,0)
+        else:
+            pagomen=round(auxval*1.25,0)
+    else:
+        auxval = auxval * 13
+        print("cotizacion: todo riesgo")
+        print("")
+        print("->empresa 1: $",round(auxval*1.25,0))
+        print("->empresa 2: $",round(auxval*1.5,0))
+        print("->empresa 3: $",round(auxval*1,0))
+        print("")
+        opcionem=validarRango(1,3, "elija tipo de empresa (1,2,3): ")
+        if opcionem == 1:
+            pagomen=round(auxval*1.25,0)
+        elif opcionem == 2:
+            pagomen=round(auxval*1.5,0)
+        else:
+            pagomen=round(auxval*1,0)
+    #Una vez elegida una opcion, se guarda la empresa y el pago por mes en las listas
+    LiEm.append(opcionem)
+    LiPa.append(pagomen)
+    #PROVICIONAL: se imprime el valor recien cargado para verificar
+    print(LiEm[cont])
+    print(LiPa[cont])
+
+
+
+
+def poliza(LiDni, LiMod, LiVa, LiPa, LiEm):
+    print("")
+    print("")
+    print("==========================================================================")
+    print("***POLIZA***")
+    print("==========================================================================")
+    print("\tDNI        MODELO                              VALOR VEHICULO    PAGO MENSUAL    EMPRESA")
     
+    for i in range(len(LiDni)):
+        print("\t%-10d %-35s $%14.2f $%13.2f %10s" % (LiDni[i], LiMod[i], LiVa[i], LiPa[i], LiEm[i]))
+        
+##############################################
 def main():
-    
-    dni=[]
+    #Preinicializacion de listas:
+        #Listas con los modelos
     motos=["moto: Harley-Davidson Road Glide Special", "moto: Ducati Panigale V4", "moto: BMW R 1250 GS ", "moto: Kawasaki Ninja ZX-10R", "moto: Yamaha MT-07", "moto: Honda CRF450R"]
     autos=["auto: Toyota Corolla", "auto: Honda Civic", "auto: Ford Mustang", "auto: Chevrolet Silverado", "auto: Tesla Model 3", "auto: BMW Serie 3"]
     camion=["camion: Scania R Series", "camion: Mercedes-Benz Actros", "camion: Volvo FH16", "camion: MAN TGX", "camion: Freightliner Cascadia", "camion: DAF XF"]
+    ##########################################
+        #Listas con los datos de los clientes
+    dni=[]
+    modelo=[]
     valorVehiculo=[]
     empresas=[]
-    nrocli=0
     pagoxmes=[]
-    tipoSeguro=[]
-
+    ##########################################
+    #Contador para saber posicion de la lista que esta siendo cargada en cada momento
+    nrocli=0
+    #Mensaje de vienvenida del programa
     ingresoProductor()
+    #While principal
+    #Condicion de finalizacion del while main -> variable opcion
     opcion=validarRango(1,3, "Por favor, ingrese 1 para cargar datos de clientes, 2 para ver listas o 3 para generar informe y salir: ")
-    
     while opcion != 3:
         if opcion==1:
-            
             ingresoCliente(dni,nrocli,motos,autos,camion,modelo,valorVehiculo)
             cotizacion(valorVehiculo,empresas,nrocli,pagoxmes)
+            #Luego de completar la carga incrementa el contador de posicion
             nrocli+=1
-            poliza(dni,modelo,valorVehiculo,pagoxmes,empresas)
-        opcion=validarRango(1,3, "Por favor, ingrese 1 para cargar datos de clientes, 2 para ver listas o 3 para generar informe y salir: ")
-            
+        #Finalizacion del ciclo while, nuevamente condicion de finalizacion
+        opcion=validarRango(1,3, "Por favor, ingrese 1 para cargar datos de clientes, 2 para ver listas o 3 para generar informe y salir: ")    
 
 if __name__ == "__main__":
     main()
